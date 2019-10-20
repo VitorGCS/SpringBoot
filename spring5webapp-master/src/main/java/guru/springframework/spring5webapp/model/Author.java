8d05956dc9d7ec074816dc67e09348c7f66acfb4
Vitor Gil Curto da Silva <35468738+VitorGCS@users.noreply.github.com>
2019-10-20 20:38:54 +0100

    .

--- spring5webapp-master/src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ spring5webapp-master/src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -9,9 +9,9 @@ public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String firstName;
-    private String lastName;
+    private Long id ;
+    private String firstName ;
+    private String lastName ;
 
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
